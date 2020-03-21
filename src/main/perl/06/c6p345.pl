5118be1a8c4aaa893b485f71b6279847bec6e971
moqi <moqimoqidea@gmail.com>
2020-03-21 11:54:55 +0800

    Update

---
+++ src/main/perl/06/c6p345.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+# 打印具体的报错信息
+use diagnostics;
+use Time::HiRes 'time';
+
+my $times_todo = 1000000;
+my $test_string = "abababdedfg";
+
+my $count = $times_todo;
+my $start_time = time();
+while ($count-- > 0) {
+    $test_string =~ m/^(a|b|c|d|e|f|g)+$/;
+}
+my $end_time = time();
+printf("Alternation takes %.3f seconds.\n", $end_time - $start_time);
+
+
+$count = $times_todo;
+$start_time = time();
+while ($count-- > 0) {
+    $test_string =~ m/^[a-g]+$/;
+}
+$end_time = time();
+printf("Character class takes %.3f seconds.\n", $end_time - $start_time);
+# Alternation takes 0.571 seconds.
+# Character class takes 0.194 seconds.
